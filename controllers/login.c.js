025bd8a45c82a432d19944b3682c57868bd33e48
Joseeee11 <115356746+Joseeee11@users.noreply.github.com>
2023-04-19 16:31:01 -0400

    Generar token

--- controllers/login.c.js
+++ controllers/login.c.js
@@ -1,14 +1,15 @@
 const loginModel = require("../models/login.m")
 const bcryptjs = require("bcryptjs")
+const loginH = require("../helpers/login.h")
 
 class login {
     validar(parametro){
         //Vamos a ver si el usuario existe en la base de datos
         return new Promise((resolve, reject) => {
             loginModel.verificaUser(parametro.usuario) 
-            .then (async (resultado_Json) => {
+            .then (async (results) => {
 
-                const confirmado = await bcryptjs.compare(parametro.contrasena, resultado_Json.contrasena)
+                const confirmado = await bcryptjs.compare(parametro.contrasena, results.contrasena)
                 
                 console.log(confirmado); // Dice si es false o true
                 // El if de abajo es para cuando sea false mande ese resolve (luego lo cambio por reject)
@@ -16,8 +17,9 @@ class login {
                     return resolve("error en la contraseña")
                 }
 // AQUIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
-
-                resolve("confirmado")
+                const resultado = loginH.tockeLogin(parametro)
+                
+                resolve(resultado)
             })
             .catch ((error) => {
                 reject(error)
