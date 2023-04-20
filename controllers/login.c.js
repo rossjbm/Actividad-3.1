42c786232f6c053f8b8734a5f6e8fb8749ad094e
Joseeee11 <118561364+SamTempest@users.noreply.github.com>
2023-04-20 18:35:12 -0400

    mas bugs, imposibles

--- controllers/login.c.js
+++ controllers/login.c.js
@@ -1,16 +1,16 @@
 const loginModel = require("../models/login.m")
 const bcryptjs = require("bcryptjs")
 const loginH = require("../helpers/login.h")
+const cookie = require('cookie')
 
 class login {
     validar(parametro){
+        console.log('Controladores');
         //Vamos a ver si el usuario existe en la base de datos
         return new Promise((resolve, reject) => {
             loginModel.verificaUser(parametro.usuario) 
             .then (async (results) => {
-
                 const confirmado = await bcryptjs.compare(parametro.contrasena, results.contrasena)
-                
                 console.log(confirmado); // Dice si es false o true
                 // El if de abajo es para cuando sea false mande ese resolve (luego lo cambio por reject)
                 if (!confirmado) {
@@ -19,21 +19,21 @@ class login {
                 if (parametro.rol != results.rol) {
                     return reject("Rol incorrecto")
                 }
-// AQUIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
                 const resultado = loginH.tockeLogin(parametro)
-                
-                resolve(resultado)
+                const Galleta = cookie.serialize('GalletaDeToken', resultado, {
+                    httpOnly: true,
+                    secure: process.env.VERI =="produccion",
+                    sameSite:'strict',
+                    maxAge: '30m',
+                    path:'/'
+
+                })
+                resolve(Galleta)
             })
             .catch ((error) => {
                 reject(error)
             })
-
         })
-        
-            
-        
-
-
     }
 }
 
--- routes/login.r.js
+++ routes/login.r.js
@@ -3,16 +3,20 @@ var router = express.Router();
 var login = require("../controllers/login.c");
 const { error } = require('console');
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.status(200).render('login', { title: 'INICIAR' });
 });
+
 router.post('/', function(req, res, next) {
+  console.log('Postllegdao');
   const {usuario, contrasena, rol} = req.body
   const parametro = {usuario, contrasena, rol}
 
   console.log(parametro);
   login.validar(parametro)
+  
   .then ((resultado) => {
     console.log(resultado)
     res.status(200).redirect('/');
