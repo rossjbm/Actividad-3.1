6d793f1ea90b6e0b312b32d3f9c99f3165b24558
Joseeee11 <115356746+Joseeee11@users.noreply.github.com>
2023-04-20 01:54:16 -0400

    Nuevo verifica el de según el rol

--- middleware/login.mid.js
+++ middleware/login.mid.js
@@ -6,7 +6,6 @@ class verificador3000{
         try{
             const token= req.headers.authorization.split(' ').pop()
             var sellado = await Verificadorneitor3000.verificarToken(token) 
-            console.log(sellado);
             if(sellado == null){
                 res.status(404)
                 return res.send("Token Invalido")
@@ -19,8 +18,28 @@ class verificador3000{
             res.status(404)
             return res.send(error)
         }
-         
-        
+    }  
+     async verificadorDeRol(req, res, next){
+        console.log("llega");
+        try{
+            const token= req.headers.authorization.split(' ').pop()
+            var sellado = await Verificadorneitor3000.verificarToken(token)
+            if(sellado == null){
+                res.status(404)
+                return res.send("Token Invalido")
+            }
+            if (sellado.rol = 'solicitante') {
+                res.status(404)
+                return res.send("No tienes el rol necesario")
+            }
+            return next()
+            
+        }catch(error) {
+            console.log('error al verificar');
+            console.log(error);
+            res.status(404)
+            return res.send(error)
+        }
     }
 }
 module.exports = new verificador3000();
